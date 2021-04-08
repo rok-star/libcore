66852b7e20e62e8e9ebf53934d06ca5d3e8e75a4
Roman Kirsanov <kirsanov2008@gmail.com>
2021-04-08 18:08:41 +0300

    any, tests

--- test/any.test.c
+++ test/any.test.c
@@ -49,23 +49,58 @@ void _any_test(void) {
 	{
 		_any_t* any = _any_create_null();
 		_ASSERT(_any_type(any) == _NULL_ANY_TYPE);
+
 		_any_set_string(any, DUMMY_STRING, strlen(DUMMY_STRING));
 		_ASSERT(_any_type(any) == _STRING_ANY_TYPE);
 		_ASSERT(strcmp(_any_string(any), DUMMY_STRING) == 0);
+
 		_any_set_number(any, 777);
 		_ASSERT(_any_type(any) == _NUMBER_ANY_TYPE);
 		_ASSERT(_any_number(any) == 777);
+
 		_any_set_date(any, DUMMY_DATE);
 		_ASSERT(_any_type(any) == _DATE_ANY_TYPE);
 		_ASSERT(_any_date(any) == DUMMY_DATE);
+
 		_any_set_array(any);
 		_ASSERT(_any_type(any) == _ARRAY_ANY_TYPE);
 		_ASSERT(_any_array_count(any) == 0);
+
 		_any_set_map(any);
 		_ASSERT(_any_type(any) == _MAP_ANY_TYPE);
 		_ASSERT(_any_map_has(any, "one") == false);
 		_ASSERT(_any_map_has(any, "two") == false);
 		_ASSERT(_any_map_has(any, "three") == false);
+
+		_any_set_string(any, DUMMY_STRING, strlen(DUMMY_STRING));
+		_ASSERT(_any_type(any) == _STRING_ANY_TYPE);
+		_ASSERT(strcmp(_any_string(any), DUMMY_STRING) == 0);
+
+		_any_set_date(any, DUMMY_DATE);
+		_ASSERT(_any_type(any) == _DATE_ANY_TYPE);
+		_ASSERT(_any_date(any) == DUMMY_DATE);
+
+		_any_set_number(any, 777);
+		_ASSERT(_any_type(any) == _NUMBER_ANY_TYPE);
+		_ASSERT(_any_number(any) == 777);
+
+		_any_destroy(any);
+	}
+
+	// String...
+	{
+		_any_t* any = _any_create_string(DUMMY_STRING, strlen(DUMMY_STRING));
+		_ASSERT(_any_type(any) == _STRING_ANY_TYPE);
+		_ASSERT(strcmp(_any_string(any), DUMMY_STRING) == 0);
+
+		_any_set_string(any, DUMMY_STRING, 5);
+		_ASSERT(_any_type(any) == _STRING_ANY_TYPE);
+		_ASSERT(strcmp(_any_string(any), "Hello") == 0);
+
+		_any_set_string(any, DUMMY_STRING, 0);
+		_ASSERT(_any_type(any) == _STRING_ANY_TYPE);
+		_ASSERT(strcmp(_any_string(any), "") == 0);
+
 		_any_destroy(any);
 	}
 }
